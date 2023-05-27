9cd0f1c0b9d3aa4244981c083e9796c57cb89774
Krysna Rachmat Ramadhan <krysnarachmat1@gmail.com>
2023-05-28 02:40:50 +0700

    first commit

---
+++ resources/views/dashboard/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard 2</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{asset('admin-asset/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{asset('admin-assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('admin-assets/dist/css/adminlte.min.css')}}">
+    </head>
+    
+    <body>    
+        @include('dashboard.layouts.navbar')
+
+        @include('dashboard.layouts.sidebar')
+        @yield('content')
+        @include('dashboard.layouts.footer')
+
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{asset('admin-assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('admin-assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('admin-assets/dist/js/adminlte.js')}}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="{{asset('admin-assets/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
+<script src="{{asset('admin-assets/plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('admin-assets/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
+<script src="{{asset('admin-assets/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('admin-assets/plugins/chart.js/Chart.min.js')}}"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('admin-assets/dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('admin-assets/dist/js/pages/dashboard2.js')}}"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('landing.pages.home.index');
+});
+
+Route::get('/about', function () {
+    return view('landing.pages.about.index');
+});
+Route::get('/contact', function () {
+    return view('landing.pages.contact.index');
+});
+
+Route::get('/wakmin', function () {
+    return view('dashboard.home');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
